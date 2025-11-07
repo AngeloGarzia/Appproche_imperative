6e3d4f2267bc3826691585c2961adc122ebae349
angelo <angelo.garzia@hotmail.fr>
2025-11-07 12:25:00 +0100

    V1 TP3

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    byte bytVar=100;
+    short shoVar=10100;
+    int intVar=1000000000;
+    long longVar=1100L;
+    float floVar=12.2f;
+    double doubVAr=12202d;
+    char charVar='a';
+    boolean booVar=true;
+    String striVar="Salut";
+    String randomString="voici le resultat d'un calcul :\n 1.5=6";
+
+    System.out.print(bytVar+" "+shoVar+" "+intVar+" "+longVar+" "+floVar+" "+doubVAr+" "+charVar+" "+booVar+" "+striVar);
+
+    System.out.print("\n"+randomString);
+
+
+    }
+}
